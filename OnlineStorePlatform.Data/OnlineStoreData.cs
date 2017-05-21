93eb2e76251ec019d0eba601131c83cb766425d7
ivolekov <ivo_lekov@abv.bg>
2017-05-21 04:07:58 +0300

    Add repository

---
+++ OnlineStorePlatform.Data/OnlineStoreData.cs
@@ -0,0 +1,27 @@
+﻿namespace OnlineStorePlatform.Data
+{
+    using OnlineStorePlatform.Data.Interfaces;
+    using OnlineStorePlatform.Data.Repositories;
+    using OnlineStorePlatform.Models.EntityModels;
+    public class OnlineStoreData : IOnlineStoreData
+    {
+        private readonly IOnlineStoreDbContext context;
+        public OnlineStoreData(IOnlineStoreDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IRepository<User> Users => new Repository<User>(this.context);
+        public IRepository<Product> Products => new Repository<Product>(this.context);
+        public IRepository<Category> Categories => new Repository<Category>(this.context);
+        public IRepository<Article> Articles => new Repository<Article>(this.context);
+        public IRepository<Customer> Customers => new Repository<Customer>(this.context);
+        public IRepository<Log> Logs => new Repository<Log>(this.context);
+        public IRepository<Order> Orders => new Repository<Order>(this.context);
+        public IOnlineStoreDbContext Context => this.context;
+        public int SaveChanges()
+        {
+            return this.context.SaveChanges();
+        }
+    }
+}
---
+++ OnlineStorePlatform.Data/Repositories/Repository.cs
@@ -0,0 +1,62 @@
+﻿namespace OnlineStorePlatform.Data.Repositories
+{
+    using Models.EntityModels;
+    using OnlineStorePlatform.Data.Interfaces;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using System.Linq.Expressions;
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected IDbSet<T> EntityTable;
+
+        public Repository(IOnlineStoreDbContext context)
+        {
+            this.EntityTable = context.Set<T>();
+        }
+        public void InsertOrUpdate(T entity)
+        {
+            this.EntityTable.AddOrUpdate(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            this.EntityTable.Remove(entity);
+        }
+
+        public IQueryable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return this.EntityTable.Where(predicate);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return this.EntityTable;
+        }
+        public IQueryable<T> Take(int count)
+        {
+            return this.EntityTable.Take(count);
+        }
+        public T GetById(int id)
+        {
+            return this.EntityTable.Find(id);
+        }
+
+        public T FindByPredicate(Expression<Func<T, bool>> predicate)
+        {
+            return this.EntityTable.FirstOrDefault(predicate);
+        }
+
+        public T GetByStringId(string id)
+        {
+            return this.EntityTable.Find(id);
+        }
+
+        public T SingleOrDefault(Expression<Func<T, bool>> predicate)
+        {
+            return this.EntityTable.FirstOrDefault();
+        }
+    }
+}
