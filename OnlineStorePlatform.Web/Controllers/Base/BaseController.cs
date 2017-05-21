abe605fe614ed3c2fbccd47cc6576a290395b465
ivolekov <ivo_lekov@abv.bg>
2017-05-21 04:16:46 +0300

    Add custom attribute for page error

---
+++ OnlineStorePlatform.Web/Controllers/Base/BaseController.cs
@@ -0,0 +1,14 @@
+﻿namespace OnlineStorePlatform.Web.Controllers.Base
+{
+    using OnlineStorePlatform.Data.Interfaces;
+    using System.Web.Mvc;
+
+    public class BaseController : Controller
+    {
+        private IOnlineStoreData data;
+        protected BaseController(IOnlineStoreData data)
+        {
+            this.data = data;
+        }
+    }
+}
