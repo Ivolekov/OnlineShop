f49de7c83c38f0c279f8e51a40701122db02ed21
ivolekov <ivo_lekov@abv.bg>
2017-05-28 17:53:12 +0300

    Change email for sent orders

--- OnlineStorePlatform.Service/EmailSettings.cs
+++ OnlineStorePlatform.Service/EmailSettings.cs
@@ -2,12 +2,11 @@
 {
     public class EmailSettings
     {
-        //TODO: Take google app pass to send email
-        public string MailToAddress = "ifreshbg@gmail.com";
-        public string MailFromAddress = "ifreshbg@gmail.com";
+        public string MailToAddress = "kidstoreazure@gmail.com";
+        public string MailFromAddress = "kidstoreazure@gmail.com";
         public bool UseSsl = true;
-        public string Username = "ifreshbg@gmail.com";
-        public string Password = "hgnmooedjjwfekpj"; // Create your own google app password, In the video I have shown you how to create app password.
+        public string Username = "kidstoreazure@gmail.com";
+        public string Password = "dmebjbczjkpyrqcs";//"hgnmooedjjwfekpj"; // Create your own google app password.
         public string ServerName = "smtp.gmail.com";
         public int ServerPort = 587;
     }
